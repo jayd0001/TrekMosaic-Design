b2c157546e3f96775a1a42753404768a2e4ae7a7
Jaideep Singh <jay61226@gmail.com>
2023-12-06 21:10:08 +0530

    Hero Component implementation, along with some other component structure

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
+import Hero from "./Hero";
+import Guide from "./Guide";
+import Camp from "./Camp";
+import Features from "./Features";
+import GetApp from "./GetApp";
 
-export { Navbar, Footer, Button };
+export { Navbar, Footer, Button, Hero, Camp, Guide, Features, GetApp };
